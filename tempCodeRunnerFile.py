f37f91a371776071a5978724553861f18e91a8f4
srimoyee6969 <118544009+srimoyee6969@users.noreply.github.com>
2024-10-08 00:20:42 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+
+description:{jd}
+
+I want the response in one single string having the structure
+{{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
+"""
+
+## streamlit app
+st.title("Smart ATS")
